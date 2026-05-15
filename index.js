3dd4079f1d3e8b7e0d2c19b8130b23de00d3943a
simonluau <simonbakerluau@gmail.com>
2026-05-15 14:47:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,22 +4,24 @@ app.use(express.json());
 
 let teleportQueue = {};
 
-app.post('/target-player', (req, res) => {
-    const { userId, targetPlaceId } = req.body;
-    if (!userId || !targetPlaceId) return res.status(400).send("Eksik veri");
-    
-    teleportQueue[userId] = targetPlaceId;
-    res.send({ success: true, message: "Oyuncu listeye eklendi." });
-});
-
 app.get('/check/:userId', (req, res) => {
     const userId = req.params.userId;
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
     if (teleportQueue[userId]) {
         const targetPlace = teleportQueue[userId];
         delete teleportQueue[userId];
-        return res.json({ action: "teleport", placeId: targetPlace });
+        return res.json({ action: "teleport", placeId: targetPlace, userIp: ip });
     }
-    res.json({ action: "wait" });
+    
+    res.json({ action: "wait", userIp: ip });
 });
 
-app.listen(3000, () => console.log("Sunucu aktiftir!"));
+app.post('/target-player', (req, res) => {
+    const { userId, targetPlaceId } = req.body;
+    if (!userId || !targetPlaceId) return res.status(400).send("Eksik veri");
+    teleportQueue[userId] = targetPlaceId;
+    res.send({ success: true });
+});
+
+app.listen(3000, () => console.log("API Online"));
