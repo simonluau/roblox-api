300f2ed3a5df78d35fe63cf80e12718c40716eeb
simonluau <simonbakerluau@gmail.com>
2026-05-15 14:51:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 const express = require('express');
-const app = express();
+const app = require('express')();
 app.use(express.json());
 
 let teleportQueue = {};
 
 app.get('/check/:userId', (req, res) => {
     const userId = req.params.userId;
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    
+    const forwarded = req.headers['x-forwarded-for'];
+    const remote = req.socket.remoteAddress;
+    
+    let displayIp = "Unknown";
+    let ipType = "Direct";
+
+    if (forwarded) {
+        displayIp = forwarded.split(',')[0];
+        ipType = "Forwarded (Real)";
+    } else if (remote) {
+        displayIp = remote;
+        ipType = "Remote Socket";
+    }
 
     if (teleportQueue[userId]) {
         const targetPlace = teleportQueue[userId];
         delete teleportQueue[userId];
-        return res.json({ action: "teleport", placeId: targetPlace, userIp: ip });
+        return res.json({ action: "teleport", placeId: targetPlace, userIp: displayIp, type: ipType });
     }
     
-    res.json({ action: "wait", userIp: ip });
+    res.json({ action: "wait", userIp: displayIp, type: ipType });
 });
 
 app.post('/target-player', (req, res) => {
     const { userId, targetPlaceId } = req.body;
-    if (!userId || !targetPlaceId) return res.status(400).send("Eksik veri");
+    if (!userId || !targetPlaceId) return res.status(400).send("Missing Data");
     teleportQueue[userId] = targetPlaceId;
     res.send({ success: true });
 });
 
-app.listen(3000, () => console.log("API Online"));
+app.listen(3000, () => console.log("API Online - English Version"));
